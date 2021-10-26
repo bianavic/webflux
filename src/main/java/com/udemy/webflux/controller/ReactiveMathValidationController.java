b6ffb7f20d40f313dd2f0185c96a4d7d7672180b
Fabiana <fferlin@uolinc.com>
2021-10-25 21:01:44 -0300

    assignment with no exception, no error signal and no controller advice

--- src/main/java/com/udemy/webflux/controller/ReactiveMathValidationController.java
+++ src/main/java/com/udemy/webflux/controller/ReactiveMathValidationController.java
@@ -1,10 +1,10 @@
 package com.udemy.webflux.controller;
 
-import com.udemy.webflux.dto.InputFailedValidationResponse;
 import com.udemy.webflux.dto.ResponseDTO;
 import com.udemy.webflux.exception.InputValidationException;
 import com.udemy.webflux.service.ReactiveMathService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -40,6 +40,16 @@ public class ReactiveMathValidationController {
                 })
                 .cast(Integer.class)
                 .flatMap(i -> this.reactiveMathService.findSquare(i));
+    }
+
+    // no exception, no error signal, no controller advice
+    @GetMapping("square/{input}/assignment")
+    public Mono<ResponseEntity<ResponseDTO>> assignment(@PathVariable int input) {
+        return Mono.just(input)
+                .filter(i -> i >= 10 && i <= 20)
+                .flatMap(i -> this.reactiveMathService.findSquare(i))
+                .map(i -> ResponseEntity.ok(i))
+                .defaultIfEmpty(ResponseEntity.badRequest().build());
     }
 
 }
