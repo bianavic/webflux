b69167934d5fe3ed13e3c802efbefd02a6385e4e
bianavic <bianavic@gmail.com>
2021-10-30 19:03:59 -0300

    remove imports and add requests predicates

--- src/main/java/com/udemy/webflux/config/RouterConfig.java
+++ src/main/java/com/udemy/webflux/config/RouterConfig.java
@@ -2,16 +2,16 @@ package com.udemy.webflux.config;
 
 import com.udemy.webflux.dto.InputFailedValidationResponse;
 import com.udemy.webflux.exception.InputValidationException;
+import java.util.function.BiFunction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.RequestPredicates;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
-
-import java.util.function.BiFunction;
 
 /**
  * @author fferlin
@@ -27,21 +27,22 @@ public class RouterConfig {
     public RouterFunction<ServerResponse> highLevelRouter() {
 
         return RouterFunctions.route()
-                .path("router", this::serverResponseRouterFunction)
-                .build();
+            .path("router", this::serverResponseRouterFunction)
+            .build();
     }
 
     //@Bean
     private RouterFunction<ServerResponse> serverResponseRouterFunction() {
 
         return RouterFunctions.route()
-                .GET("square/{input}", requestHandler::squareHandler)
-                .GET("table/{input}", requestHandler::tableHandler)
-                .GET("table/{input}/stream", requestHandler::tableStreamHandler)
-                .POST("multiply", requestHandler::multiplyHandler)
-                .GET("square/{input}/validation", requestHandler::squareHandlerWithValidation)
-                .onError(InputValidationException.class, exceptionhandler())
-                .build();
+            .GET("square/{input}", RequestPredicates.path("*/1?").or(RequestPredicates.path("*/20")),requestHandler::squareHandler)
+            .GET("square/{input}", req -> ServerResponse.badRequest().bodyValue("only 10-19 allowed"))
+            .GET("table/{input}", requestHandler::tableHandler)
+            .GET("table/{input}/stream", requestHandler::tableStreamHandler)
+            .POST("multiply", requestHandler::multiplyHandler)
+            .GET("square/{input}/validation", requestHandler::squareHandlerWithValidation)
+            .onError(InputValidationException.class, exceptionhandler())
+            .build();
     }
 
     private BiFunction<Throwable, ServerRequest, Mono<ServerResponse>> exceptionhandler() {
